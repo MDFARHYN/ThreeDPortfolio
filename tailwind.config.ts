7e0626d08c09d5d81715b1440066aae5febea323
Farhan <tusarcc@gmail.com>
2024-11-04 22:58:59 +0600

    cmt

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -119,8 +119,8 @@ export default config
 
 // This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
 function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
  
